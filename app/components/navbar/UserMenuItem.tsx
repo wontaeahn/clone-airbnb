cbb6bbc3d500b5acfd7fe6f6b51ed100216c4b58
wontaeahn <awt0523@gmail.com>
2023-09-25 21:41:13 +0900

    Create Navbar UI

---
+++ app/components/navbar/UserMenuItem.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface UserMenuItemProps {
+    onClick: () => void;
+    label: string;
+}
+
+const UserMenuItem: React.FC<UserMenuItemProps> = ({ onClick, label }) => {
+    return (
+        <div
+            onClick={onClick}
+            className=" px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+        >
+            {label}
+        </div>
+    );
+};
+
+export default UserMenuItem;
